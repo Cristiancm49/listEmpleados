f1c2bea9c2b4cebc444232ae21da18c34e53eef9
cristiancm49 <ccortesm2000@gmail.com>
2024-11-24 21:53:32 -0500

    se agrega backend

---
+++ backend/backend/models/Departamento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend.Models
+{
+    public partial class Departamento
+    {
+        public Departamento()
+        {
+            Empleados = new HashSet<Empleado>();
+        }
+
+        public int IdDepartaemtno { get; set; }
+        public string? Nombre { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+
+        public virtual ICollection<Empleado> Empleados { get; set; }
+    }
+}
